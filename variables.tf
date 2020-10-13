ca47893fa6f5dfdc47c7be343afcce81fea4a90d
Federico <federico.silvestre@vtex.com>
2020-10-13 19:03:14 +0200

    First commit

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_cidr"    {}
+variable "tag_name"    {}
+variable "zones"       {}
+variable "region"      {}
+variable "bucket"      {}
+variable "github_user" {}
+variable "rate"        {}
